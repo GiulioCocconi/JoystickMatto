9a0f9c7ff8853c829c2da141bbd6b95b93663153
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2021-01-24 17:02:45 +0100

    Create Joystick.h

---
+++ Joystick.h
@@ -0,0 +1,22 @@
+#ifndef __JOYSTICK_h
+#define __JOYSTICK_h
+
+#include "Arduino.h"
+
+class Joystick {
+    public:     
+                Joystick(int, int, int);
+                void calibra();
+                int getX();
+                int getY();
+                int getXStd();
+                int getYStd();
+    private:    
+                int pinX;
+                int pinY;
+                int pinZ;
+                int dX;
+                int dY;
+};
+
+#endif
