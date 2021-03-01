f3b6a9aa7fcc38ee89d588b9f0de9dd8d1df9385
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2021-03-01 13:03:25 +0100

    Update JoystickMatto.h
    Aggiunti i commenti

--- JoystickMatto.h
+++ JoystickMatto.h
@@ -5,13 +5,13 @@
 
 class JoystickMatto {
     public:     
-                JoystickMatto(int, int);
-                void calibra();
-                int getX();
-                int getY();
-                double getXStd();
-                double getYStd();
-                double getAngle();
+                JoystickMatto(int pinX, int pinY);
+                void calibra(); //Setta i valori dX e dY in modo che il joy risulti una circonferenza centrata sull'intersezione degli assi cartesiani
+                int getX(); //Restituisce il valore raw di X
+                int getY(); //Restituisce il valore raw di Y
+                double getXStd(); //Restituisce il valore di X tenendo conto di dX
+                double getYStd(); //Restituisce il valore di Y tenendo conto di dY
+                double getAngle(); //Mediante arcos determina l'angolo tra l'asse x e il punto P di cordinate (X,Y) [Richiede dX e dY]
     private:    
                 int pinX;
                 int pinY;
