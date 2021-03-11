6b9a2e7a08233f2c7a5d39635c99071acc52a604
Giulio Cocconi <31406038+GiulioCocconi@users.noreply.github.com>
2021-03-11 21:31:02 +0100

    Update JoystickMatto.cpp

--- JoystickMatto.cpp
+++ JoystickMatto.cpp
@@ -13,20 +13,17 @@ void JoystickMatto::calibra() {
   this->dY = analogRead(this->pinY);
  }
  
-int JoystickMatto::getXRaw() {
+int JoystickMatto::getX() {
   return analogRead(this->pinX);
  }
 
-int JoystickMatto::getX() {
-  return -1 * this->getXRaw();
-}
  
 double JoystickMatto::getXStd() {
   if (this->dX == 0) {
     //this->calibra();
     return this->getX();
   }
-  return (double)(analogRead(this->pinX) - dX)/dX;
+  return (double) -1 * (this->getX() - dX)/dX;
 }
 
 int JoystickMatto::getY() {
@@ -38,7 +35,7 @@ int JoystickMatto::getY() {
     //this->calibra();
     return this->getY();
   }
-  return (double)(analogRead(this->pinY) - dY)/dY;
+  return (double)(this->getY() - dY)/dY;
 }
 
 double JoystickMatto::getAngle() {
@@ -46,6 +43,9 @@ double JoystickMatto::getAngle() {
   double y = this->getYStd();
   double ipo = (double)sqrt(x*x+y*y);
   double algo = (double)acos(x/ipo);
+  if (x == 0 && y == 0) {
+    return 0;
+  }
   if (y>=0) {
     return algo;
   }
